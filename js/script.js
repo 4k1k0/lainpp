b64877add933d9964e925f6982f718434350585c
Akko <dulo.rodrigo@gmail.com>
2017-05-19 01:04:43 +0000

    Bug fix: Center the images in the lightbox
    I added another for loop to load all the images. That way the
    variables h(eight) and w(idth) can use the correct values the first
    time the pointer is on a image.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 function lightbox(){
     var img = document.getElementsByClassName('post-image');
     var link, w, h;
-    
+
+    /*
+     * Load all the big images in the thread
+     * this way the width and height can be used
+     * to set the margin the first time the pointer
+     * is on an image
+    */
+    for (var i = 0; i < img.length; i++) {
+      link = img[i].parentElement.href;
+      document.getElementById('thread').insertAdjacentHTML('afterbegin', '<img class="lightbox-img-hide" src="' + link + '">');
+    }
+
     for(var i = 0; i < img.length; i++){
         /*
          *  When the pointer is on an image: load image into a lightbox,
@@ -12,20 +23,20 @@ function lightbox(){
             link = this.parentElement.href;
             document.getElementById('thread').insertAdjacentHTML('afterbegin', '<div id="lightbox"><img id="lightbox-img" src="' + link + '"></div>');
             var lightboxImg = document.getElementById('lightbox-img');
-            w = lightboxImg.width;
             h = lightboxImg.height;
-            lightboxImg.setAttribute('style', 'margin: -' + h/2 + 'px 0px 0px -' + w/2 + 'px;');
+            w = lightboxImg.width;
+            lightboxImg.style.margin = '-' + h/2 + 'px 0px 0px -' + w/2 + 'px';
         }, false);
-        
+
         /*
          *  When pointer is out: Remove the lightbox from the DOM
          */
         img[i].addEventListener('mouseout', function(){
             document.getElementById('lightbox').parentNode.removeChild(document.getElementById('lightbox'));
         }, false);
-        
+
     }
-    
+
 }
 
 function expandIMG(){
@@ -34,11 +45,12 @@ function expandIMG(){
      *  in the bottom bar which can be used
      *  to load all the original images in the thread
     */
+
     var bottomBar = document.getElementsByClassName('threadlinks');
     for(var i = 0; i < bottomBar.length; i++){
         bottomBar[i].insertAdjacentHTML('afterbegin', '<span class="threadlink"><a id="expand-img">[  Expand images  ]  </a></span>');
     }
-    
+
     var botton = document.getElementById('expand-img');
     var img = document.getElementsByClassName('post-image');
     botton.addEventListener('click', function(){
