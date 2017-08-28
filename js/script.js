1848b77029446dc84276061b01ea891791aef015
Akko <dulo.rodrigo@gmail.com>
2017-08-27 20:27:22 -0500

    Fix "Expand images" bug
    This prevents non images files to be load when the user clicks on the "Expand images" button.

--- js/script.js
+++ js/script.js
@@ -1,84 +1,95 @@
 function lightbox(){
-    let img = document.getElementsByClassName('post-image');
+  let img = document.getElementsByClassName('post-image');
+
+  /*
+   * Load all the big images in the thread
+  */
+  for (let i = 0; i < img.length; i++) {
+    let link = img[i].parentElement.href;
+    document.getElementById('thread').insertAdjacentHTML(
+      'afterbegin', '<img class="lightbox-img-hide" src="' + link + '">'
+    );
+  }
+
+  for(let i = 0; i < img.length; i++){
+    /*
+     *  When the pointer is on an image
+     *  the image appears in the lightbox
+    */
+    img[i].addEventListener('mouseover', function(){
+      let link = this.parentElement.href;
+      let ext = link.slice(-4, link.length);
+      switch (ext) {
+        case '.png':
+        case '.jpg':
+        case 'jpeg':
+        case '.gif':
+        case '.bmp':
+          document.getElementById('thread').insertAdjacentHTML(
+            'afterbegin',
+            '<div id="lightbox"><img src="' + link + '"></div>'
+          );
+          break;
+        case '.pdf':
+          break;
+        default:
+          let file = link.indexOf('&');
+          link = link.slice(0, file);
+          let url = decodeURI(link);
+          let limit = url.indexOf('v=/');
+          url = 'https://lainchan.org/' + url.slice(limit + 3, url.length);
+          document.getElementById('thread').insertAdjacentHTML(
+            'afterbegin',
+            '<div id="lightbox"><video src="' + url + '" autoplay="true" loop="true"></video></div>'
+          );
+          break;
+      }
+    }, false);
 
     /*
-     * Load all the big images in the thread
+     * When pointer is out: Remove the lightbox from the DOM
     */
-    for (let i = 0; i < img.length; i++) {
-      let link = img[i].parentElement.href;
-      document.getElementById('thread').insertAdjacentHTML(
-        'afterbegin', '<img class="lightbox-img-hide" src="' + link + '">'
-      );
-    }
-
-    for(let i = 0; i < img.length; i++){
-        /*
-         *  When the pointer is on an image
-         *  the image appears in the lightbox
-        */
-        img[i].addEventListener('mouseover', function(){
-            let link = this.parentElement.href;
-            let ext = link.slice(-4, link.length);
-            switch (ext) {
-              case '.png':
-              case '.jpg':
-              case 'jpeg':
-              case '.gif':
-              case '.bmp':
-                document.getElementById('thread').insertAdjacentHTML(
-                  'afterbegin',
-                  '<div id="lightbox"><img src="' + link + '"></div>'
-                );
-                break;
-              case '.pdf':
-                break;
-              default:
-                let file = link.indexOf('&');
-                link = link.slice(0, file);
-                let url = decodeURI(link);
-                let limit = url.indexOf('v=/');
-                url = 'https://lainchan.org/' + url.slice(limit + 3, url.length);
-                document.getElementById('thread').insertAdjacentHTML(
-                  'afterbegin',
-                  '<div id="lightbox"><video src="' + url + '" autoplay="true" loop="true"></video></div>'
-                );
-                break;
-            }
-        }, false);
-
-        /*
-         * When pointer is out: Remove the lightbox from the DOM
-        */
-        img[i].addEventListener('mouseout', function(){
-            document.getElementById('lightbox').parentNode.removeChild(document.getElementById('lightbox'));
-        }, false);
-
-    }
-
+    img[i].addEventListener('mouseout', function(){
+      document.getElementById('lightbox').parentNode.removeChild(document.getElementById('lightbox'));
+    }, false);
+  }
 }
 
 function expandIMG(){
-    /*
-     * It creates a button
-     * in the bottom bar which can be used
-     * to load all the original images in the thread
-    */
+  /*
+   * It creates a button
+   * in the bottom bar which can be used
+   * to load all the original images in the thread
+  */
 
-    let bottomBar = document.getElementsByClassName('threadlinks');
-    for(let i = 0; i < bottomBar.length; i++){
-        bottomBar[i].insertAdjacentHTML('afterbegin', '<span class="threadlink"><a id="expand-img">[  Expand images  ]  </a></span>');
+  let bottomBar = document.getElementsByClassName('threadlinks');
+  for(let i = 0; i < bottomBar.length; i++){
+    bottomBar[i].insertAdjacentHTML(
+      'afterbegin',
+      '<span class="threadlink"><a id="expand-img">[  Expand images  ]  </a></span>'
+    );
+  }
+
+  let botton = document.getElementById('expand-img');
+  let img = document.getElementsByClassName('post-image');
+  botton.addEventListener('click', function(){
+    for(let i = 0; i < img.length; i++){
+      let link = img[i].parentElement.href;
+      let ext = link.slice(-4, link.length);
+      switch (ext) {
+        case '.png':
+        case '.jpg':
+        case 'jpeg':
+        case '.gif':
+        case '.bmp':
+          img[i].click();
+          break;
+      }
     }
-
-    let botton = document.getElementById('expand-img');
-    let img = document.getElementsByClassName('post-image');
-    botton.addEventListener('click', function(){
-        for(let i = 0; i < img.length; i++){
-            img[i].click();
-        }
-    });
+  });
 }
 
 window.addEventListener('load', function(){
-    lightbox();
-    expandIMG();
+  lightbox();
+  expandIMG();
 });
